38d041d392028af8e9768fcc500c51414ae98792
AchyuthNamburi <117505569+AchyuthNamburi@users.noreply.github.com>
2026-07-13 10:58:03 +0530

    Time: 15 ms (50.39%) | Memory: 26.4 MB (20.60%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-    unordered_map<string, int> dp; // here we are using unordered_map ...since we have negative values of curr_sum also . 
     
-
-    int solve(vector<int>& nums, int target, int i, int curr_sum) {
+    int S;
+    int solve(vector<int>& nums, int target, int i, int curr_sum,vector<vector<int>>& dp) {
 
         if (i == nums.size()) {
             return (curr_sum == target);
         }
 
-        string key = to_string(i) + "_" + to_string(curr_sum); // adding _ to make sure that no conflicts bet the idx and curr_sum
+        if(dp[i][curr_sum+S]!=-1){ // observe we are doing +S to handle the negative values of curr_sum
+            return dp[i][curr_sum+S];
+        }
 
-        if (dp.find(key) != dp.end())
-            return dp[key];
+        int plus = solve(nums, target, i + 1, curr_sum + nums[i],dp);
+        int minus = solve(nums, target, i + 1, curr_sum - nums[i],dp);
 
-        int plus = solve(nums, target, i + 1, curr_sum + nums[i]);
-        int minus = solve(nums, target, i + 1, curr_sum - nums[i]);
-
-        return dp[key] = plus + minus;
+        return dp[i][curr_sum+S] = plus + minus;
     }
 
     int findTargetSumWays(vector<int>& nums, int target) {
-        return solve(nums, target, 0, 0);
+        int n=nums.size();
+
+        S=accumulate(nums.begin(),nums.end(),0); // find total sum 
+
+        vector<vector<int>> dp(n+1,vector<int>(2*S+1,-1)); // observe we took 2times the size of the curr_sum to handle the negative cases of curr_sum
+        return solve(nums, target, 0, 0,dp);
     }
 };
