09810b81683b3709fbac664fa6892e469e6588b7
AchyuthNamburi <117505569+AchyuthNamburi@users.noreply.github.com>
2026-07-23 18:33:00 +0530

    Time: 2 ms (22.98%) | Memory: 8.4 MB (22.72%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    // Hint we will have only 2 cases !
+    // either it will stay in LOOP or it will converge to 1 
+    // if it is converging to 1 then it is Happy
+    //else it is not happy
+
+    int solve(int x){
+        int sum=0;
+        while(x){
+            int last=x%10;
+            sum+=pow(last,2);
+            x/=10;
+        }
+
+        return sum;
+    }
+    bool isHappy(int n) {
+
+        unordered_map<int,int> mp;
+
+        while(n!=1){
+            if(mp.find(n)!=mp.end()){
+                return false;
+            }
+            else{
+                mp[n]++;
+            }
+            n=solve(n);
+        }
+
+        return true;
+    }
+};
