1d744f02b1c2f75ff1d8523fc649c76da8a49d18
AchyuthNamburi <117505569+AchyuthNamburi@users.noreply.github.com>
2026-07-25 19:58:31 +0530

    Time: 375 ms (44.40%) | Memory: 8.7 MB (94.56%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,37 +1,27 @@
 class Solution {
 public:
     bool issafe(vector<vector<char>>& board,int row,int col,char ch){
-        //check row
-        for(int j=0;j<=9;j++){
-            if(board[row][j]==ch){
-                return false;
-            }
-        }
-        //check col
-        for(int i=0;i<9;i++){
-            if(board[i][col]==ch){
-                return false;
-            }
-        }
-        //for 3*3 grid
-        int startRow=(row/3)*3;
-        int startCol=(col/3)*3;
+        for (int i = 0; i < 9; i++) {
+            // row check
+            if (board[row][i] == ch) return false;
 
-        for(int i=startRow; i<startRow+3; i++){
-            for(int j=startCol; j<startCol+3; j++){
-                if(board[i][j]==ch){
-                    return false;
-                }
-            }
-        }
+            // column check
+            if (board[i][col] == ch) return false;
 
+            // 3x3 sub-box check
+            int r = 3 * (row / 3) + i / 3;  // crucial ..observe properly /
+            int c = 3 * (col / 3) + i % 3;  //crucial ....observe properly %
+            if (board[r][c] == ch) return false;
+        }
         return true;
     }
     bool solve(vector<vector<char>>& board){
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
+                
                 if(board[i][j]=='.'){
                     for(char ch='1'; ch<='9' ;ch++){
+                        
                         if(issafe(board,i,j,ch)){
                             board[i][j]=ch; //assign
                             if(solve(board)) return true; //recurse
