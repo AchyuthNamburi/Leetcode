18c9f7e233523b9495ca9dfbe5d75600357bb181
AchyuthNamburi <117505569+AchyuthNamburi@users.noreply.github.com>
2026-07-31 13:58:09 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (46.43%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,21 +1,49 @@
 class Solution {
 public:
-    //Boyer --Moore  voting Algorithm
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        int n=nums.size();
+        int candidate1 = 0, candidate2 = 1;
+        int count1 = 0, count2 = 0;
 
-        for(auto it : nums){
-            mp[it]++;
-        }
-
-        vector<int> res;
-        for(auto it : mp){
-            if(it.second>floor(n/3)){
-                res.push_back(it.first);
+        //1st pass - Find potential candidates
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            }
+            else if (num == candidate2) {
+                count2++;
+            }
+            else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            }
+            else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            }
+            else {
+                count1--;
+                count2--;
             }
         }
+        // 2nd Pass-Verify the candidates
+        count1 = 0;
+        count2 = 0;
 
-        return res;
+        for (int num : nums) {
+            if (num == candidate1)
+                count1++;
+            else if (num == candidate2)
+                count2++;
+        }
+
+        vector<int> ans;
+
+        if (count1 > nums.size() / 3) //integer division automatically takes the floor.
+            ans.push_back(candidate1);
+
+        if (count2 > nums.size() / 3)
+            ans.push_back(candidate2);
+
+        return ans;
     }
 };
