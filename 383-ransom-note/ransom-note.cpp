0705eaf97cfe078471e93ed7c21f52d51cd753a0
AchyuthNamburi <117505569+AchyuthNamburi@users.noreply.github.com>
2026-08-03 00:26:12 +0530

    Time: 9 ms (11.49%) | Memory: 12 MB (29.83%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> mp;
+
+        for(auto it:magazine){
+            mp[it]++;
+        }
+
+        for(auto it:ransomNote){
+            if(mp.find(it)==mp.end() || mp[it]==0){
+                return false;
+            }
+            mp[it]--;
+            // if(mp[it]==0) mp.erase();
+        }
+
+        return true;
+    }
+};
