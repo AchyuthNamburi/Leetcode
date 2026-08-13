8bf2dd80bb3fcc70e7f01245a886434d6fb53fb7
Achyuth Krishna <117505569+AchyuthNamburi@users.noreply.github.com>
2026-08-13 20:45:44 +0530

    Time: 12 ms (82.49%) | Memory: 26.7 MB (16.96%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -5,35 +5,33 @@ public:
     };
 
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        // BFS returns the shortest Path ---don't worry and excite !
        int n = grid.size();
         int x_src = 0,y_src = 0;
         int x_tar = n-1,y_tar = n-1;
         if(grid[x_src][y_src] != 0 || grid[x_tar][y_tar] != 0) return -1;
-        queue<pair<int,int>>q;
+        queue<pair<pair<int,int>,int>>q; // stores {x_new,y_new}{steps}
         vector<vector<int>>vis(n,vector<int>(n,0));
-        int steps = 1;
-        q.push({0,0});
+        q.push({{0,0},1}); // we have to count the src also
         vis[0][0] = 1;
         while(!q.empty()){
-            int size = q.size();
-            while(size--){
-                int x = q.front().first;
-                int y = q.front().second;
+                int x = q.front().first.first;
+                int y = q.front().first.second;
+                int curr_steps=q.front().second;
                 q.pop();
-                if(x == n-1 && y == n-1 && grid[x][y] == 0) return steps;
+                if(x == n-1 && y == n-1 && grid[x][y] == 0) return curr_steps;
                 for(int k=0;k<c.size();k++){
                     int x_new = x + c[k][0];
                     int y_new = y + c[k][1];
                     if(x_new >=0 && y_new >=0 && x_new < n
                     && y_new < n && grid[x_new][y_new] == 0 && !vis[x_new][y_new])
                     {
-                        vis[x_new][y_new] = 1;
-                        q.push({x_new,y_new});
+                        vis[x_new][y_new] = 1; 
+                        q.push({{x_new,y_new},curr_steps+1});
                     }
                 }
         }
-            steps++;
-        }
+            
         return -1;
     }
 };
