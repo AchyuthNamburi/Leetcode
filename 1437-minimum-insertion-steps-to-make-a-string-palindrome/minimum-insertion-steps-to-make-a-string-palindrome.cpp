96a6f698a0b611f9e7ac899d6f12d7c0d21da1e9
Achyuth Krishna <117505569+AchyuthNamburi@users.noreply.github.com>
2026-08-21 15:38:43 +0530

    Time: 35 ms (46.93%) | Memory: 31.6 MB (38.85%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-    int minInsertions(string s) {
-        //simple way is to find the LCS of string s and the reverse(s)
-        
-        string temp=s;
-        reverse(s.begin(),s.end());
-
-        //find the LCS bet this string s and rev string
-        int n=s.length();
-
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
-
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                if(s[i-1]==temp[j-1]){
-                    dp[i][j]=dp[i-1][j-1]+1;
-                }
-                else{
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-                }
-                
-            }
+    int solve(string& s, int i, int j, vector<vector<int>>& dp) {
+        // Base cases
+        if(i >= j) {
+            return 0;  // Empty or single character is already palindrome
         }
-
-        return n-dp[n][n];
+        
+        if(dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        
+        // If characters match, move inward
+        if(s[i] == s[j]) {
+            dp[i][j] = solve(s, i+1, j-1, dp);
+        } else {
+            // Option 1: Insert at beginning to match s[j]
+            // Option 2: Insert at end to match s[i]
+            dp[i][j] = 1 + min(solve(s, i+1, j, dp),    // Insert s[i] at position j+1
+                               solve(s, i, j-1, dp));    // Insert s[j] at position i-1
+        }
+        
+        return dp[i][j];
+    }
+    
+    int minInsertions(string s) {
+        int n = s.length();
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        return solve(s, 0, n-1, dp);
     }
 };
