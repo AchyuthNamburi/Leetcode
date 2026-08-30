c52254f0527b1da8a22348d0ee18d0cd65dabde2
Achyuth Krishna <117505569+AchyuthNamburi@users.noreply.github.com>
2026-08-30 19:57:31 +0530

    Time: 29 ms (24.08%) | Memory: 48.4 MB (68.91%) - LeetSync

---
+++ 816-design-hashset/design-hashset.cpp
@@ -0,0 +1,54 @@
+class MyHashSet {
+public:
+    vector<list<int>> v;
+    int size=1000;
+    MyHashSet() {
+        v.resize(size);
+        
+    }
+    
+    void add(int key) {
+        int bucket_no=key%size;
+        auto& chain=v[bucket_no];
+        for(auto it =chain.begin();it!=chain.end();it++){
+            if(*it==key){
+                return;
+            }
+        }
+       chain.push_back(key);
+
+    }
+    
+    void remove(int key) {
+        int bucket_no=key%size;
+        auto& chain=v[bucket_no];
+        for(auto it= chain.begin();it!=chain.end();it++){
+            if(*it==key){
+                chain.erase(it);
+                return;
+            }
+        }
+       
+    }
+    
+    bool contains(int key) {
+        int bucket_no=key%size;
+        auto& chain=v[bucket_no];
+        for(auto it=chain.begin();it!=chain.end();it++){
+            if(*it==key){
+                
+                return true;
+            }
+        }
+        return false;
+        
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
