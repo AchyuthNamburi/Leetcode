d78f4d18bfa5f4a07e25ea16188559f8372beb34
Achyuth Krishna <117505569+AchyuthNamburi@users.noreply.github.com>
2026-09-02 17:06:25 +0530

    Time: 772 ms (5.02%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1161-project-employees-i/project-employees-i.sql
@@ -0,0 +1,5 @@
+select project_id,
+    round( sum(E.experience_years) * 1.0 / count(E.employee_id),2) as average_years 
+from Project P 
+join Employee E on P.employee_id=E.employee_id
+group by P.project_id;
