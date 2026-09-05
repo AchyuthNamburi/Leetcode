27571d1f82d5d7798307c61a0a7b37503780fa72
Achyuth Krishna <117505569+AchyuthNamburi@users.noreply.github.com>
2026-09-05 19:54:37 +0530

    Time: 8 ms (13.78%) | Memory: 136.8 MB (58.11%) - LeetSync

--- 4270-minimum-operations-to-make-array-non-decreasing/minimum-operations-to-make-array-non-decreasing.cpp
+++ 4270-minimum-operations-to-make-array-non-decreasing/minimum-operations-to-make-array-non-decreasing.cpp
@@ -1,15 +1,36 @@
 class Solution {
 public:
-    long long minOperations(vector<int>& nums) {
+    bool ispossible(int mid,vector<int>& nums){
         int n=nums.size();
-        long long sum=0;
-
         for(int i=1;i<n;i++){
-            if(nums[i]<nums[i-1]){
-                sum+=nums[i-1]-nums[i];
-            }
+            if(nums[i]<nums[i-1]) return false;
         }
 
-        return sum;
+        return true;
+    }
+    long long minOperations(vector<int>& nums) {
+        int n=nums.size();
+
+        long long  low=0;
+        long long max_diff=0;
+        for(int i=1;i<n;i++){
+            if(nums[i]<nums[i-1]){
+                max_diff+=nums[i-1]-nums[i];
+            }
+        }
+        long long high=max_diff;
+
+        // int high=*max_element(nums.begin(),nums.end())-*min_element(nums.begin(),nums.end());
+
+        while(low<high){
+            long long  mid=low+(high-low)/2;
+            if(ispossible(mid,nums)){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return low;
     }
 };
