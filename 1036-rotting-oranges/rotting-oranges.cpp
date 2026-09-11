881ba4320965d0edd9ac1912a3f7b5fc0ed462e0
Achyuth Krishna <117505569+AchyuthNamburi@users.noreply.github.com>
2026-09-11 19:43:07 +0530

    Time: 0 ms (100.00%) | Memory: 17 MB (44.96%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,39 +1,55 @@
 class Solution {
-public:
-vector<vector<int>> coordinates = {{-1,0},{1,0},{0,1},{0,-1}};
+public: 
+    vector<vector<int>> dir={{0,1},{1,0},{-1,0},{0,-1}};
     int orangesRotting(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        // bfs on grid
-        queue<pair<int,int>>q;
-        int fresh = 0;
+        int m=grid.size();
+        int n=grid[0].size();
+
+        queue<pair<int,int>> q;
+        int fresh=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j] == 2)
-                    q.push({i,j});
-                else if(grid[i][j] == 1)
+                if(grid[i][j]==2){
+                    q.push({i,j}); // push all the rotten into queue
+                }
+                else if(grid[i][j]==1){
                     fresh++;
+                }
             }
         }
-        int steps = 0;
-        while(!q.empty() && fresh > 0){
-            int size = q.size();
-            while(size--){ // multi source BFS
-                auto [x,y] = q.front();
+
+        int timer=0;
+        while(!q.empty() && fresh>0){
+            int size=q.size();
+
+            while(size--){
+                auto it =q.front();
+                int x=it.first;
+                int y=it.second;
                 q.pop();
-                for(int k = 0;k<coordinates.size();k++){
-                    int x_new = x + coordinates[k][0];
-                    int y_new = y + coordinates[k][1];
-                    if(x_new >=0 && y_new >=0 && x_new < m && y_new < n && grid[x_new][y_new] == 1){
-                    grid[x_new][y_new] = 2; // rot it
-                    q.push({x_new,y_new});
-                    fresh--;
+
+                for(int k=0;k<4;k++){
+                    int new_x=x+dir[k][0];
+                    int new_y=y+dir[k][1];
+
+                    if(new_x>=0 && new_x<m && new_y>=0 && new_y<n && grid[new_x][new_y]==1){
+                        q.push({new_x,new_y});
+                        grid[new_x][new_y]=2; // make it rotten
+                        fresh--;
                     }
                 }
             }
-            steps++;
+            timer++;
         }
-        if(fresh == 0) return steps;
-        else return -1;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    return -1; // if any fresh oranges are left
+                }
+            }
+        }
+
+        return timer;
     }
 };
